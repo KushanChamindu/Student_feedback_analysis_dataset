c30949668c616a38bfec53296beb651f27ba8298
Kushan <ra.kushan1997@gmail.com>
2022-01-18 11:26:27 +0530

    add dataset

--- code_for_read_annotaed_data.py
+++ code_for_read_annotaed_data.py
@@ -14,7 +14,7 @@ annotator_1 = [['Annotator_1','Annotated_part_1','Final_Dataset_tsv'],['Annotato
 annotator_2=[['Annotator_3','Annotated_part_6','Final_Dataset_tsv']]
 annotator_3 = [['Annotator_2','Annotated_part_3','Final_Dataset_tsv'],['Annotator_2','Annotated_part_4','ratemy_professor_data_from_sorted_list_shuffle_1'],['Annotator_2','Annotated_part_5','additional_100_of_rate_my_proffesor']]
 annotators = [annotator_1,annotator_2,annotator_3]
-path_for_folder = "/content/drive/Shareddrives/FYP/Annotated Data/"    ## change this path to Annotated Data folder
+path_for_folder = "./Annotated Student Feedback Data/"    ## change this path to "Annotated Student Feedback Data" folder
 
 with open(path_for_folder+'Annotator_3/Annotated_part_6/TypeSystem.xml', 'rb') as f: # get all annotated tagsets
   typesystem_tmp = load_typesystem(f)
